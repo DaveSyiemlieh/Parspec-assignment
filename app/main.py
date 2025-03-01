cec496730942cdc9c3790a333c07c3cae614ea85
Dave <davy.syiem7@gmail.com>
2025-03-01 23:25:37 +0530

    Fix bugs

--- app/main.py
+++ app/main.py
@@ -1,30 +1,49 @@
 from fastapi import FastAPI, Depends
-from .models import CreateOrderRequest, MetricsResponse, StatusCount
-from .order_repository import OrderRepository
+# from order_repository import OrderRepository
+from pydantic import BaseModel
+from typing import List
 
+#---------------------------- Models ----------------------------#
+class CreateOrderRequest(BaseModel):
+    user_id: int
+    order_id: int
+    item_ids: List
+    total_amount: int
+
+class StatusCount(BaseModel):
+    pending_count: int
+    processing_count: int
+    completed_count: int
+
+class MetricsResponse(BaseModel):
+    total_orders: int
+    average_processing_time: float
+    status_counts: StatusCount
+#------------------------------------------------------------------#
+
+
+#---------------------------- APIs --------------------------------#
 app = FastAPI()
 
-# Ideally, route-logic would be kept in a route-directory but to simplify the submission of the assignment,
-# the logic will be placed in 1 file
+db_session = None
 @app.post("/order")
-def create_orders(req: CreateOrderRequest, session: Depends(psl_session)):
+def create_orders(req: CreateOrderRequest):
     # Create order
-    # Offload processing to queue
-    order_repository = OrderRepository(session)
-
-    order_respository.create_order(req)
+    # Offload processing to queue``
+    # order_repository = OrderRepository(db_session)
+    # order_respository.create_order(req)
     
-    return {"content": "Ordering..."}
+    return {"content": "Ordering 2..."}
 
 
 @app.get("{user_id}/orders")
-def orders_status(user_id: int, session: Depends(psl_session)):
+def orders_status(user_id: int):
     # Get all orders for user
     return {"content": f"Order status for {user_id=}"}
 
 
 @app.get("/metrics")
-def metrics(session: Depends(psl_session)):
+def metrics():
     #   - Total number of orders processed.
     #   - Average processing time for orders.
     #   - Count of orders in each status -
@@ -33,11 +52,13 @@ def metrics(session: Depends(psl_session)):
     #     - Completed
 
     return MetricsResponse(
-        total_orders = 0
-        average_processing_time =0
+        total_orders = 0,
+        average_processing_time = 0,
         status_counts = StatusCount(
-            pending_count: int
-            processing_count: int
-            completed_count: int
+            pending_count = 0,
+            processing_count = 0,
+            completed_count = 0
         )
     )
+#------------------------------------------------------------------#
+
